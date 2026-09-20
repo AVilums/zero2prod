b47d321c393987948d4f604b91664f58b84ffe84
Artūrs Viļums <arturvilum@gmail.com>
2026-09-20 20:14:58 +0300

    4.3

--- src/startup.rs
+++ src/startup.rs
@@ -1,6 +1,7 @@
 use std::net::TcpListener;
 use actix_web::dev::Server;
 use actix_web::{web, App, HttpServer};
+use actix_web::middleware::Logger;
 use sqlx::PgPool;
 use crate::routes::{health_check, subscribe};
 
@@ -12,6 +13,7 @@ pub fn run(
     let db_pool = web::Data::new(db_pool);
     let server = HttpServer::new(move || {
         App::new()
+            .wrap(Logger::default())
             .route("/health", web::get().to(health_check))
             .route("/subscribe", web::post().to(subscribe))
             .app_data(db_pool.clone())
